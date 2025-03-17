4be2d42b2d36a49ece617b950c39fdcf2a62f8b4
R-Kri <rubrangsokri@gmail.com>
2025-03-17 16:45:34 +0530

    updated

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,7 +3,7 @@ const API_BASE_URL = "https://dashboard-server-sable.vercel.app";
 export const fetchData = async () => {
   const response = await fetch(`${API_BASE_URL}/api/admin`);
   if (!response.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error("Failed to fetch the data");
   }
   return response.json();
 };
